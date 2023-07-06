6bf870ab86411585f98ff13218b4ea063c1acb1a
nvmarzakov <nikolay.marzakov@gmail.com>
2023-07-06 23:27:05 +0300

    configure settings.py

--- petstagram_workshop/settings.py
+++ petstagram_workshop/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'petstagram_workshop.common',
+    'petstagram_workshop.accounts',
+    'petstagram_workshop.pets',
+    'petstagram_workshop.photos',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'staticfiles',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
