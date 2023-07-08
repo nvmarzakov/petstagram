bce5fb5ad096691bb06e8796ea663cd76de1a745
nvmarzakov <nikolay.marzakov@gmail.com>
2023-07-08 10:36:19 +0300

    update pet model and admin site

--- petstagram_workshop/pets/models.py
+++ petstagram_workshop/pets/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
+class Pet(models.Model):
+    MAX_LEN_NAME = 30
+
+    name = models.CharField(
+        max_length=MAX_LEN_NAME,
+    )
+
+    personal_photo = models.URLField(
+
+    )
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    slug = models.SlugField(
+        # new
+        unique=True,
+        editable=False,
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.slug:
+            self.slug = slugify(f"{self.name}--{self.id}")
+            return super().save(*args, **kwargs)
--- petstagram_workshop/settings.py
+++ petstagram_workshop/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'petstagram_workshop.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "petstagram-db",
+        "USER": "marzaka",
+        "PASSWORD": "Kosmonaft1",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
