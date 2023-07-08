71ae5b1135794e5ce5d6b0c7c0fb27a6d2b795b3
nvmarzakov <nikolay.marzakov@gmail.com>
2023-07-08 10:47:38 +0300

    set database, update models

--- petstagram_workshop/photos/models.py
+++ petstagram_workshop/photos/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
+from petstagram_workshop.pets.models import Pet
+
 
 # Create your models here.
+class Photo(models.Model):
+    MAX_LEN_DESCRIPTION = 300
+    MIN_LEN_DESCRIPTION = 10
+    MAX_LEN_LOCATION = 30
+
+    photo = models.ImageField()
+
+    description = models.TextField(
+        max_length=MAX_LEN_DESCRIPTION,
+        validators=(MinLengthValidator(MIN_LEN_DESCRIPTION),)
+    )
+
+    location = models.CharField(
+        max_length=MAX_LEN_LOCATION,
+    )
+
+    tagged_pets = models.ManyToManyField(
+        Pet, blank=True,
+    )
+
+    date_of_publication = models.DateField(
+        auto_now=True,
+    )
