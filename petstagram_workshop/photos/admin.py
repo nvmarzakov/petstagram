efe765feca78caf6fbb1a913bf15c100e5969847
nvmarzakov <nikolay.marzakov@gmail.com>
2023-07-24 11:49:12 +0300

    admin update

--- petstagram_workshop/photos/admin.py
+++ petstagram_workshop/photos/admin.py
@@ -6,4 +6,11 @@ from petstagram_workshop.photos.models import Photo
 # Register your models here.
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('pk', 'date_of_publication', 'pets')
+
+    @staticmethod
+    def pets(self, current_photo_obj):
+        tagged_pets = current_photo_obj.tagged_pets.all()
+        if tagged_pets:
+            return ', '.join(p.name for p in tagged_pets)
+        return 'No pets'
