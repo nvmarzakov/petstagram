adc375eba4297a0bea3935b87c59b3a356a02529
nvmarzakov <nikolay.marzakov@gmail.com>
2023-07-24 12:22:29 +0300

    creating the like model

--- petstagram_workshop/common/models.py
+++ petstagram_workshop/common/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from petstagram_workshop.photos.models import Photo
+
+
 # Create your models here.
+class PhotoComment(models.Model):
+    MAX_TEXT_LENGTH = 300
+
+    text = models.CharField(
+        max_length=MAX_TEXT_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    publication_date_and_time = models.DateTimeField(
+        auto_now=True,
+        blank=True,
+        null=False,
+    )
+
+    photo = models.ForeignKey(
+        Photo,
+        on_delete=models.RESTRICT,
+        null=False,
+        blank=True,
+    )
+
+
+class PhotoLike(models.Model):
+    photo = models.ForeignKey(
+        Photo,
+        on_delete=models.RESTRICT,
+        null=False,
+        blank=True,
+    )
+
+
+
+    # When we have users
+    # user = models.ForeignKey(
+    #     User
+    # )
