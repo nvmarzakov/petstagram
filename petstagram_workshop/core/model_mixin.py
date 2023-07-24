9f0fffb7fa23d3bea60083ce5f1cca8f2d39da6a
nvmarzakov <nikolay.marzakov@gmail.com>
2023-07-24 11:36:40 +0300

    model mixin update

--- petstagram_workshop/core/model_mixin.py
+++ petstagram_workshop/core/model_mixin.py
@@ -2,6 +2,5 @@ class StrFromFieldsMixin:
     str_fields = ()
 
     def __str__(self):
-        return ';'.join(
-            f'{str_field}={getattr(self, str_field, None)}' for str_field in self.str_fields
-        )
+        fields = [(str_field, getattr(self, str_field, None)) for str_field in self.str_fields]
+        return ', '.join(f'{name}={value}' for (name, value) in fields)
